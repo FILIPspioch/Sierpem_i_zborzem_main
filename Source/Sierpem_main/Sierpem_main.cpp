2b3228e2fc2787045e95b6e0d588c77c33326713
FilipSpioch <biedaczeq@gmail.com>
2024-04-13 21:20:22 +0200

    Initial commit

---
+++ Source/Sierpem_main/Sierpem_main.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sierpem_main.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sierpem_main, "Sierpem_main" );
