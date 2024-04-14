b7f5523769b946ae9f3addb8c8411165b55040c0
Brajn2 <brajank29@gmail.com>
2024-04-14 23:20:52 +0200

    Ostatnia szansa

---
+++ Sianem_i_Sierpem_VAM/Source/Sianem_i_Sierpem_VAM/MainGameCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainGameCharacter.h"
+
+// Sets default values
+AMainGameCharacter::AMainGameCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMainGameCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMainGameCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMainGameCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Sianem_i_Sierpem_VAM/Source/Sianem_i_Sierpem_VAM/Sianem_i_Sierpem_VAM.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sianem_i_Sierpem_VAM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sianem_i_Sierpem_VAM, "Sianem_i_Sierpem_VAM" );
